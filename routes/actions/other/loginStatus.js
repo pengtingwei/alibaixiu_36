6ac62de4aa71b119a65b3116eb9a75dbe077a950
pengtingwei <358003445@qq.com>
2019-08-29 16:38:32 +0800

    完成个人信息功能

--- routes/actions/other/loginStatus.js
+++ routes/actions/other/loginStatus.js
@@ -1,6 +1,7 @@
 module.exports = async (req, res) => {
 	if (req.session && req.session.userInfo) {
-		res.send('var isLogin = true')
+		const s = `var isLogin = true; var userId=\"${req.session.userInfo._id}\"`
+		res.send(s)
 	}else {
 		res.send('var isLogin = false')
 	}
