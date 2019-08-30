93a3e7e565d6f8e7bc692655aa0d9bdafecc8e9a
pengtingwei <358003445@qq.com>
2019-08-30 11:47:31 +0800

    完成前台文章分类功能

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -1,9 +1,9 @@
 // 向服务器端发送请求 索要随机推荐数据
 $.ajax({
-	type: 'get',
-	url: '/posts/random',
-	success: function (response) {
-		var randomTpl = `
+    type: 'get',
+    url: '/posts/random',
+    success: function (response) {
+        var randomTpl = `
 			{{each data}}
 			<li>
 			  <a href="detail.html?id={{$value._id}}">
@@ -16,7 +16,29 @@ $.ajax({
 			</li>
 			{{/each}}
 		`;
-		var html = template.render(randomTpl, {data: response});
-		$('#randomBox').html(html)
-	}
+        var html = template.render(randomTpl, { data: response });
+        $('#randomBox').html(html)
+    }
+})
+
+
+// 向服务器端发送请求 索要文章分类列表数据
+$.ajax({
+    type: 'get',
+    url: '/categories',
+    success: function (response) {
+        var navTpl = `
+			{{each data}}
+			<li>
+				<a href="list.html?categoryId={{$value._id}}">
+					<i class="fa {{$value.className}}"></i>{{$value.title}}
+				</a>
+			</li>
+			{{/each}}
+		`;
+        var html = template.render(navTpl, { data: response });
+        // $('#navBox').html(html)
+        // $('#topNavBox').html(html)
+        $('.nav_item').html(html);
+    }
 })
